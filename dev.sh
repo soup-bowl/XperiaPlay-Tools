1d5c508b740e4fab407953ba87b40ee582fd694b
soup-bowl <code@soupbowl.io>
2021-03-17 21:25:22 +0000

    Moved the dev downloader back.

--- dev.sh
+++ dev.sh
@@ -4,5 +4,5 @@
 # https://www.mediafire.com/folder/729cecjag0n9l/Sony_Xperia_Play
 # This only needs to be downloaded once - please do not excessively re-download these files.
 curl -o ./xpt-dev.zip https://files.soupbowl.io/xpt-dev.zip
-unzip -o xpt-dev.zip
+unzip -o xpt-dev.zip -d ./resources
 rm xpt-dev.zip
